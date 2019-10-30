602967ea229025b8fc1ddcbca8325f0149cda8fe
JC <james.calingo@gmail.com>
2019-10-30 10:45:32 -0400

    changed page title

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -3,7 +3,7 @@ import { StyledCell } from "./styles/StyledCell";
 import { TETROMINOS } from "../tetromino"
 function Cell ({ type }) {
   return(
-<StyledCell type={0} color={TETROMINOS[0].color}></StyledCell>
+<StyledCell type={0} color={TETROMINOS[0].color}>X</StyledCell>
   )
 }
 
